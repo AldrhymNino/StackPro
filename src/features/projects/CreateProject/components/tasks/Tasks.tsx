8c716b0b9d832aca70c826cf00dba2efbbb753c1
Aldrhym <aldrhym.nino3003@gmail.com>
2025-12-10 13:55:05 -0500

    .

--- src/features/projects/CreateProject/components/tasks/Tasks.tsx
+++ src/features/projects/CreateProject/components/tasks/Tasks.tsx
@@ -1,12 +1,12 @@
 import { CheckCircle2, Plus, StickyNote } from "lucide-react";
 import { useState } from "react";
-import type { task } from "../../../../../types/Project";
+// import type { task } from "../../../../../types/Project";
 
 type TasksProps = {
     styles: { [key: string]: string };
     state: {
-        tasks: task[];
-        setTasks: React.Dispatch<React.SetStateAction<task[]>>;
+        tasks: { id: string; title: string; done: boolean; }[];
+        setTasks: React.Dispatch<React.SetStateAction<{ id: string; title: string; done: boolean; }[]>>;
     }
 };
 
